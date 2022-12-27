c83edcd7722e9ec8f93da77162469bc268ec196d
Ramazan <wdire@t-x.local>
2022-12-27 20:13:32 +0300

    added seo and favoricon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,37 @@ import GlobalStyles from "@/styles/globals";
 import type { AppProps } from "next/app";
 import { WalletProvider } from "@suiet/wallet-kit";
 import "@suiet/wallet-kit/style.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <WalletProvider>
+        <Head>
+          <title>CapyWitter</title>
+          <meta name="description" content="" />
+          <meta property="og:title" content="" />
+          <meta property="og:description" content="" />
+          <meta property="og:image" content="" />
+          <meta name="keywords" content="" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/images/favoricon/apple-icon-180x180.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/images/favoricon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/images/favoricon/favicon-16x16.png"
+          />
+        </Head>
         <GlobalStyles />
         <Header />
         <Component {...pageProps} />
